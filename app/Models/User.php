cfb7c3119cb4ff1fcb7bb73f5d49f8ecbd60903f
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2024-04-16 05:34:37 +0200

    add soft delete to the UserModel & userstable

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Notifiable, LaratrustUserTrait;
+    use HasApiTokens, HasFactory, Notifiable, Notifiable, LaratrustUserTrait, SoftDeletes;
 
     protected $fillable = ['name', 'email', 'password', 'type', 'image',];
 
